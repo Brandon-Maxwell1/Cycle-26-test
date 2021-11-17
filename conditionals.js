e6469b831040cfeb2f9365678d73e7350f8d7bd6
Brandon Maxwell <theill1b22@gmail.com>
2021-11-17 14:41:04 -0600

    completed exercises in conditions, forgot to open loops, but are all in conditionals

---
+++ conditionals.js
@@ -0,0 +1,66 @@
+// NOTES:  ****Conditionals****
+// Definition: perform different operatins based on the given condition
+// Task: define wheter a numbe ris positive or negative
+// let interger = 1
+// if(interger < 0){
+// console.log("Negative: " + interger)
+// }
+// // inside the parenthesis in an if statment, we write logical statement checking whether it's true or false
+// }else{
+//     // if we didn't meet our logical statemtn, else code block will execute
+//     console.log("Positive: " + interger)
+// }
+
+
+// let age = 24
+// if(age < 18){
+//     console.log("Access Denied: " + age)
+// }
+// else{
+//     console.log("Access Granted: " + age)
+// }
+
+// let num = 10
+
+// if(num >= 100){
+//     console.log("Number is Positive or Greater than 100")
+// } else if(num < 100 && num >= 0){
+//     console.log(num + "Number is positive and less than 100")
+// } else(num 0){
+// }
+// }
+
+
+// Grade
+
+// let grade = 69
+
+// if(grade >= 90){
+//     console.log("A")
+// } else if(grade >= 80 && grade < 90){
+//     console.log("B")
+// } else if(grade >= 70 && grade < 80){
+//     console.log("C")
+// } else if(grade < 70){
+//     console.log("Fail")
+// }
+
+
+// for(i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+
+// for(let i = 10; i > 0; i--) {
+//     console.log(i)
+// }
+
+// for(let i = 1; i < 10; i++ && i++){
+//     console.log(i)
+// }
+
+// for(let i = 2; i < 11; i++ && i++){
+//     console.log(i)
+// }
+
+
